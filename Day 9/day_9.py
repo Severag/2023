e14b362a4bbcf250498b0c6df279f501598909af
Severag <odonobii@gmail.com>
2023-12-08 23:48:09 -0600

    added more comments to Day 9

--- Day 9/day_9.py
+++ Day 9/day_9.py
@@ -26,15 +26,17 @@ def solve(data, is_part1=True):
     line_idx = -1 if is_part1 else 0
     
     for line in data:
-        next_row = line[1:] - line[:-1]
+        next_row = line[1:] - line[:-1]  # differences between the previous rows' entries
+        # switches sign after each next() call
         mult = itertools.cycle([1]) if is_part1 else itertools.cycle([1,-1])
-        lasts = [next(mult) * line[line_idx], next(mult) * next_row[line_idx]]
+        # after calculating each row, just keep the relevant end
+        ends = [next(mult) * line[line_idx], next(mult) * next_row[line_idx]]
         
         while not np.all(next_row == 0):
             next_row = next_row[1:] - next_row[:-1]
-            lasts.append(next(mult) * next_row[line_idx])
+            ends.append(next(mult) * next_row[line_idx])
         
-        marginal_nums.append(sum(lasts))
+        marginal_nums.append(sum(ends))
     
     return sum(marginal_nums)
 
